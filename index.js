c803147d10bb5fcf7c8386c1af8e539fe42e7cc3
Trần Huy Hoàng <wujor01@gmail.com>
2022-09-28 10:44:03 +0700

    commit

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
 let csvToJson = require('convert-csv-to-json');
+var xlsx = require('node-xlsx');
+var fs = require('fs');
+var rows = [];
+var writeStr = "";
 
-let fileInputName = 'report_datafresh_srh.csv'; 
-let fileOutputName = 'myOutputFile.json';
+Run = async () => {
+    let inputFilename = "report_datafresh_srh.xlsx";
+    
+    let outputFilename = inputFilename.split('.')[0] + '.json';
+    if (inputFilename.includes(".xls")){
+        inputFilename = await ConvertXLStoCSV(inputFilename);
+    }
 
-csvToJson.generateJsonFileFromCsv(fileInputName,fileOutputName);
+    console.log(`Converting ${inputFilename} to JSON...`);
+    csvToJson.generateJsonFileFromCsv(inputFilename, outputFilename);
+}
+
+ConvertXLStoCSV = async (inputFilename) => {
+    console.log(`Converting ${inputFilename} to CSV...`);
+    var obj = xlsx.parse(inputFilename); // parses a file
+        //looping through all sheets
+        for(var i = 0; i < obj.length; i++)
+        {
+            var sheet = obj[i];
+            //loop through all rows in the sheet
+            for(var j = 0; j < sheet['data'].length; j++)
+            {
+                    //add the row to the rows array
+                    rows.push(sheet['data'][j]);
+            }
+        }
+    
+        //creates the csv string to write it to a file
+        for(var i = 0; i < rows.length; i++)
+        {
+            writeStr += rows[i].join(";") + "\n";//systax ";" or ","
+        }
+    
+        inputFilename = inputFilename.split('.')[0] + '.csv';
+    
+        //writes to a file, but you will presumably send the csv as a      
+        //response instead
+        await fs.writeFileSync(inputFilename, writeStr, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log(inputFilename + " was saved in the current directory!");
+        });
+
+        return inputFilename;
+}
+
+Run();
