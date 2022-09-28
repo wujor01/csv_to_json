073faae8fb67ffd28bfa3800ba5c452e830fdb73
Trần Huy Hoàng <wujor01@gmail.com>
2022-09-28 15:23:45 +0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let csvToJson = require('convert-csv-to-json');
+const { convertCsvToXlsx } = require('@aternus/csv-to-xlsx');
 var xlsx = require('node-xlsx');
 var fs = require('fs');
 var rows = [];
@@ -10,18 +11,28 @@ Run = async () => {
         console.log("Please type your's filepath want to convert to JSON!\nExample: 'npm start example.csv' OR 'npm start example.xlsx'");
         return;
     }
-    
-    let outputFilename = inputFilename.split('.')[0] + '.json';
-    if (inputFilename.includes(".xls")){
-        inputFilename = await ConvertXLStoCSV(inputFilename);
+
+    let type = process.argv.slice(3)[0];
+    if (type == 'csvtoxlsx') {
+        console.log(`Converting ${inputFilename} to XLSX...`);
+        ConvertCSVtoXLS(inputFilename);
+    }else{
+        let outputFilename = inputFilename.split('.')[0] + '.json';
+        if (inputFilename.includes(".xls")){
+            console.log(`Converting ${inputFilename} to CSV...`);
+            inputFilename = await ConvertXLStoCSV(inputFilename);
+        }
+
+        console.log(`Converting ${inputFilename} to JSON...`);
+        csvToJson.generateJsonFileFromCsv(inputFilename, outputFilename);
     }
-
-    console.log(`Converting ${inputFilename} to JSON...`);
-    csvToJson.generateJsonFileFromCsv(inputFilename, outputFilename);
 }
-
-ConvertXLStoCSV = async (inputFilename) => {
-    console.log(`Converting ${inputFilename} to CSV...`);
+/**
+ * Convert XLS to CSV
+ * @param {string} inputFilename filepath input
+ * @param {string} separate separate csv (';' or ',' or '|') default new excel view is ';'
+ */
+const ConvertXLStoCSV = async (inputFilename, separate = ';') => {
     var obj = xlsx.parse(inputFilename); // parses a file
         //looping through all sheets
         for(var i = 0; i < obj.length; i++)
@@ -34,25 +45,30 @@ ConvertXLStoCSV = async (inputFilename) => {
                     rows.push(sheet['data'][j]);
             }
         }
-    
+
         //creates the csv string to write it to a file
-        for(var i = 0; i < rows.length; i++)
-        {
-            writeStr += rows[i].join(";") + "\n";//systax ";" or ","
-        }
+        writeStr += rows[i].join(separate) + "\n";
     
         inputFilename = inputFilename.split('.')[0] + '.csv';
     
         //writes to a file, but you will presumably send the csv as a      
         //response instead
-        await fs.writeFileSync(inputFilename, writeStr, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log(inputFilename + " was saved in the current directory!");
-        });
+        await fs.writeFileSync(inputFilename, writeStr, 'utf-8');
 
         return inputFilename;
+}
+
+/**
+ * Convert CSV to XLS: ONLY RUN AS separate = ','
+ * @param {string} inputFilename filepath input
+ */
+ const ConvertCSVtoXLS = async (inputFilename) => {
+    let outputFilename = inputFilename.split('.')[0] + '.xlsx';
+    if (fs.existsSync(outputFilename)) {
+        fs.unlinkSync(outputFilename);
+    }
+
+    convertCsvToXlsx(inputFilename, outputFilename);
 }
 
 Run();
